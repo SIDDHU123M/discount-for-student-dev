2c3287c0536f5997ae949602e0896546ed040291
Sidhartha <siddharthaveldhandijb@gmail.com>
2025-07-16 04:20:24 +0530

    Fixed assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/discount-for-student-dev/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
